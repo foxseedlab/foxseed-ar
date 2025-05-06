5d121c487f24bf7dfccd4071a4614ffd9434de45
Foxseed <takara2314_TK@outlook.jp>
2025-05-06 16:54:40 +0900

    feat: Workers部分を hono に置き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useCallback, useState } from 'react';
+
 export default function App() {
+  const [message, setMessage] = useState('');
+
+  const handleClick = useCallback(() => {
+    fetch('/foo')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
   return (
     <main>
       <h1 className="mb-4 text-4xl font-bold">Coming soon...</h1>
+      <button
+        className="px-4 py-1 text-green-900 font-bold bg-[#badcad] rounded-md"
+        type="button"
+        onClick={handleClick}
+      >
+        テスト
+      </button>
+      <p>{message}</p>
+
       <small>©️ 2025 ふぉくしーど</small>
     </main>
   );
