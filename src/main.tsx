d171111636cd2cf4271ffea7ac5aa46e43dabc6d
Foxseed <takara2314_TK@outlook.jp>
2025-05-06 16:13:39 +0900

    feat: Tailwind CSS を導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const app = document.getElementById('app');
+
+if (!app) {
+  throw new Error('App element not found');
+}
+
+createRoot(app).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
