f5ef97f0f8dbdbe3f954be70513f5ed888cefe65
Foxseed <takara2314_TK@outlook.jp>
2025-05-17 00:03:14 +0900

    feat: 他のログAPIも実装

---
+++ worker/api/marker_found.ts
@@ -0,0 +1,42 @@
+import { Hono } from 'hono';
+import { drizzle } from 'drizzle-orm/d1';
+import { markerFoundLogs } from '../../src/db/schema';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
+
+type Bindings = {
+  DB: D1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+const schema = z.object({
+  guestName: z.string().min(1, 'guest name is required'),
+  markerId: z.string().min(1, 'marker id is required'),
+  deviceInnerWidth: z
+    .number()
+    .int()
+    .positive('device inner width must be a positive integer'),
+  deviceInnerHeight: z
+    .number()
+    .int()
+    .positive('device inner height must be a positive integer'),
+  userAgent: z.string().min(1, 'user agent is required'),
+});
+
+app.post('/', zValidator('json', schema), async (c) => {
+  const params = c.req.valid('json');
+  const db = drizzle(c.env.DB);
+
+  const result = await db.insert(markerFoundLogs).values({
+    guestName: params.guestName,
+    markerId: params.markerId,
+    deviceInnerWidth: params.deviceInnerWidth,
+    deviceInnerHeight: params.deviceInnerHeight,
+    userAgent: params.userAgent,
+  });
+
+  return c.json(result);
+});
+
+export default app;
--- worker/index.ts
+++ worker/index.ts
@@ -1,40 +1,12 @@
 import { Hono } from 'hono';
-import { drizzle } from 'drizzle-orm/d1';
-import { enterLogs } from '../src/db/schema';
-import { z } from 'zod';
-import { zValidator } from '@hono/zod-validator';
+import enterLogApp from './api/enter';
+import markerFoundApp from './api/marker_found';
+import characterClickApp from './api/character_click';
 
-type Bindings = {
-  DB: D1Database;
-};
+const app = new Hono();
 
-const app = new Hono<{ Bindings: Bindings }>();
-
-const enterLogSchema = z.object({
-  guestName: z.string().min(1, 'guest name is required'),
-  deviceInnerWidth: z
-    .number()
-    .int()
-    .positive('device inner width must be a positive integer'),
-  deviceInnerHeight: z
-    .number()
-    .int()
-    .positive('device inner height must be a positive integer'),
-  userAgent: z.string().min(1, 'user agent is required'),
-});
-
-app.post('/enter', zValidator('json', enterLogSchema), async (c) => {
-  const params = c.req.valid('json');
-  const db = drizzle(c.env.DB);
-
-  const result = await db.insert(enterLogs).values({
-    guestName: params.guestName,
-    deviceInnerWidth: params.deviceInnerWidth,
-    deviceInnerHeight: params.deviceInnerHeight,
-    userAgent: params.userAgent,
-  });
-
-  return c.json(result);
-});
+app.route('/enter', enterLogApp);
+app.route('/marker_found', markerFoundApp);
+app.route('/character_click', characterClickApp);
 
 export default app;
